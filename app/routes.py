767cf283de9b85d02fc2d43e2ba8069ee812a9ec
JackP900 <jpage2603@gmail.com>
2026-08-02 16:50:59 +0100

    created stuck route

--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,19 @@
 from app import app
-from flask import render_template
+from flask import render_template, request, jsonify
 
 @app.route("/")
 def index():
     return render_template("index.html")
+
+@app.route("/breakdown", methods=["POST"])
+def breakdown():
+    data = request.get_json()
+    steps = ["open a doc and title it", "write one messy sentence"]
+    return jsonify({"steps": steps})
+
+@app.route("/stuck", methods=["POST"])
+def stuck():
+    data = request.get_json()
+    step = data.get("step")
+    smaller_steps = ["open the doc", "title the doc", "write a singluar messy sentence", ]
+    return jsonify({"steps": smaller_steps, "assignment": data.get("assignment")})
