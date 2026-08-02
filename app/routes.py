9340b2e0583777f86c8aba5efcf364a8ef217904
JackP900 <jpage2603@gmail.com>
2026-08-02 22:14:49 +0100

    stuck funtion done

--- app/routes.py
+++ app/routes.py
@@ -1,5 +1,6 @@
 from app import app
 from flask import render_template, request, jsonify
+from ai import breakdown, rebreak
 
 @app.route("/")
 def index():
@@ -8,12 +9,13 @@ def index():
 @app.route("/breakdown", methods=["POST"])
 def breakdown():
     data = request.get_json()
-    steps = ["open a doc and title it", "write one messy sentence"]
+    steps = breakdown(data)
     return jsonify({"steps": steps})
 
 @app.route("/stuck", methods=["POST"])
 def stuck():
     data = request.get_json()
     step = data.get("step")
+    smaller_steps = rebreak(step, data, )
     smaller_steps = ["open the doc", "title the doc", "write a singluar messy sentence", ]
     return jsonify({"steps": smaller_steps, "assignment": data.get("assignment")})
